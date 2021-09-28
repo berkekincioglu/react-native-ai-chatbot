7e705a5604ced07b4338188f5ab3e2a6105ac3da
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-09-29 00:46:42 +0300

    layout $2

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import { Fragment } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
 
 const ChatScreen = () => {
@@ -17,18 +29,39 @@ const ChatScreen = () => {
     });
   }, [navigation]);
   return (
-    <View style={styles.container}>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.container}
+        keyboardVerticalOffset={90}
+      >
+        <Fragment>
+          <ScrollView></ScrollView>
+          <View style={styles.footer}>
+            <TextInput placeholder='Send Message' style={styles.textInput} />
+            <TouchableOpacity>
+              <Ionicons name='send' size={24} color='black' />
+            </TouchableOpacity>
+          </View>
+        </Fragment>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
 export default ChatScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { flex: 1 },
+  footer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+    borderRadius: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 5,
   },
+  textInput: { fontSize: 20 },
 });
