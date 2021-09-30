4056c13bcb9741ad8ab3e4ecdfa13451b2cb0172
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-09-30 17:13:20 +0300

    redux integration $2

--- store.js
+++ store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import chatReducer from './src/slices/chatSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    chat: chatReducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
