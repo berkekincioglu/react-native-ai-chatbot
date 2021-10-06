cd6e6043ab623b3095bb70782cb66ef53eb68e06
Berk Ekincioglu <berkekincioglu@MacBook-Pro.local>
2021-10-06 20:36:56 +0300

    chating done

--- App.js
+++ App.js
@@ -5,24 +5,24 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartScreen from './src/screens/StartScreen';
 import ChatScreen from './src/screens/ChatScreen';
 import { Provider } from 'react-redux';
-import store from './store';
+// import store from './store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name='Start'
-            component={StartScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name='Chat' component={ChatScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    // <Provider store={store}>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Start'
+          component={StartScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name='Chat' component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // </Provider>
   );
 };
 
